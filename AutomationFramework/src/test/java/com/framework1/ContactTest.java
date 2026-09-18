e582d5033beb3aba00d6c28f8f163ab8a91b489b
Qsp <Qsp@192.168.1.181>
2026-09-18 10:02:43 +0530

    commiy

--- AutomationFramework/src/test/java/com/framework1/ContactTest.java
+++ AutomationFramework/src/test/java/com/framework1/ContactTest.java
@@ -7,10 +7,14 @@ public class ContactTest {
 	@Test
 	public void createContact() {
 
+		String URL = System.getProperty("url");
 		String BROWSER = System.getProperty("browser");
 		String UN = System.getProperty("username");
+		String PWD = System.getProperty("password");
+		System.out.println(URL);
 		System.out.println(BROWSER);
 		System.out.println(UN);
+		System.out.println(PWD);
 
 		System.out.println("Execute Create contact");
 
